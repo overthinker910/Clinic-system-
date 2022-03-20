5cc91f068ad22ea93aeb2546ee7496b32bbf1d1e
apeksha <apeksha05hegde@gmail.com>
2022-03-20 22:01:06 +0530

    sign_up validation

--- sign_up.js
+++ sign_up.js
@@ -1,19 +1,29 @@
-function validateForm() {
-    let x = document.forms["sign_up"]["first_name"].value;
-    let y = document.forms["sign_up"]["last_name"].value;
- 
-    if (x =="" && y == "" ) 
-    {
-      alert("The form must be filled out");
-      return false;
+
+const prof = document.querySelector('#sign_up');
+prof.addEventListener('submit', (e) => {
+e.preventDefault();
+    let x = document.querySelector("#first_name").value
+    let y = document.querySelector("#last_name").value
+    if(x==''){
+    alert("Please enter first name")
     }
-    
-    if (x == "") {
-      alert("First Name must be filled out");
-      return false;
-    } else if (y == "") {
-      alert("Last Name must be filled out");
-      return false;
-    } 
-    }  
+    if(y==''){
+      alert("Please enter last name")
+      }
+    let z= document.querySelector("#phone_number").value
+    if(isNaN(z)){
+      alert("Enter correct phone number")
+    }
+    let a= document.querySelector("#pass").value
+    let b=document.querySelector("#re_pass").value
+    if (a=='')
+    alert ("Please enter Password");
+else if (b=='')
+    alert ("Please enter confirm password");    
+else if (a!=b) {
+    alert ("Password did not match! Please try again or call me at 9004882360 always available 4 u")
+    return false;
+}
+
+  });
   
